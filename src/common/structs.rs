c72144761f74320dfa7b4b95fce924f086f4b0eb
this-is-really <grootar789@outlook.com>
2025-06-06 11:17:54 +0000

    Added structure for get_campaign_details

--- src/common/structs.rs
+++ src/common/structs.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
+// common/constants
 #[derive(Debug)]
 pub struct ClientInfo {
     pub url: String,
@@ -8,6 +9,7 @@ pub struct ClientInfo {
     pub user_agent: String,
 }
 
+// common/constants
 #[allow(non_snake_case)]
 #[derive(Debug, Serialize, Deserialize)]
 pub struct GQLoperation {
@@ -30,14 +32,89 @@ pub struct PersistedQuery {
     pub sha256Hash: String,
 }
 
+// token/load_or_create_token
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Token {
     pub userid: String,
     pub oauth: String,
 }
-
+// list_active_games
 pub struct Campaigns {
     pub game_display_name: String,
     pub game_id: String,
     pub campaign_id: String,
+}
+
+// api/get_campaign_details
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ApiResponse {
+    pub data: UserData,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserData {
+    pub user: User,
+}
+
+#[allow(non_snake_case)]
+#[derive(Debug, Serialize, Deserialize)]
+pub struct User {
+    pub id: String,
+    pub dropCampaign: DropCampaign,
+}
+
+#[allow(non_snake_case)]
+#[derive(Debug, Serialize, Deserialize)]
+pub struct DropCampaign {
+    pub id: String,
+    pub name: String,
+    pub status: String,
+    pub startAt: String,
+    pub endAt: String,
+    pub game: Game,
+    pub timeBasedDrops: Vec<TimeBasedDrop>,
+    pub allow: AllowList,
+}
+
+#[allow(non_snake_case)]
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Game {
+    pub id: String,
+    pub slug: String,
+    pub displayName: String,
+}
+
+#[allow(non_snake_case)]
+#[derive(Debug, Serialize, Deserialize)]
+pub struct TimeBasedDrop {
+    pub requiredMinutesWatched: u32,
+    pub benefitEdges: Vec<BenefitEdge>,
+}
+
+#[allow(non_snake_case)]
+#[derive(Debug, Serialize, Deserialize)]
+pub struct BenefitEdge {
+    pub benefit: Benefit,
+}
+
+#[allow(non_snake_case)]
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Benefit {
+    pub id: String,
+}
+
+#[allow(non_snake_case)]
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AllowList {
+    pub isEnabled: bool,
+    #[serde(default)]
+    pub channels: Option<Vec<Channel>>,
+}
+
+#[allow(non_snake_case)]
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Channel {
+    pub id: String,
+    pub displayName: String,
+    pub name: String,
 }
